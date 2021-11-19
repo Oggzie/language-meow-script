cfb98d784180ae9476daddb4a92ddbd5d749fa98
Tran Hoang Nhat <saothoi@gmail.com>
2021-11-19 15:36:43 +0700

    Clean up unrelated files

--- spec/language-meow-script-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import LanguageMeowScriptView from '../lib/language-meow-script-view';
-
-describe('LanguageMeowScriptView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
